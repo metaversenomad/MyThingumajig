9bdd08fa718e188f9c0211aeeff16550da0cd698
metaversenomad <mr.oguz01@gmail.com>
2024-06-07 22:19:34 +0200

    SearchFilters

--- src/test/java/YouTubeAutomation.java
+++ src/test/java/YouTubeAutomation.java
@@ -5,17 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 public class YouTubeAutomation {
     public static void main(String[] args) throws InterruptedException {
             // Set the path to your ChromeDriver executable
-            WebDriverManager.edgedriver().setup();
+            WebDriverManager.chromedriver().setup();
             // Initialize the ChromeDriver
-            WebDriver driver = new EdgeDriver();
+            WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.youtube.com");
         driver.manage().window().maximize();
@@ -25,8 +29,25 @@ public class YouTubeAutomation {
         WebElement cookies = driver.findElement(By.xpath("//button[@class='yt-spec-button-shape-next yt-spec-button-shape-next--filled yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m']"));
         cookies.click();
 
-        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder='Search']"));
-        searchBox.sendKeys("java");
+        WebElement inputBox = driver.findElement(By.xpath("//input[@placeholder='Search']"));
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(3));
+
+        wait.until(ExpectedConditions.elementToBeClickable(inputBox));
+
+        inputBox.click();
+
+        inputBox.sendKeys("Konusanlar");
+
+        driver.findElement(By.id("search-icon-legacy")).click();
+
+        Thread.sleep(3000);
+
+        WebElement filters = driver.findElement(By.xpath("//*[@id=\"filter-button\"]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]"));
+
+        filters.click();
+
+
     }
 
 }
